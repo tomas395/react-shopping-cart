383d7cb660c17b0194ba15caa12e3ea8af4393e6
Tomas Ortiz <TomasOrtiz@protonmail.com>
2020-01-20 14:19:31 -0800

    MVP

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Item = props => {
-	return (
-		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+  return (
+    <div className="shopping-cart_item">
+      <img src={props.item.image} alt={`${props.item.title} book`} />
 
-
-			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
-			</div>
-		</div>
-	);
+      <div>
+        <h1>{props.item.title}</h1>
+        <p>$ {props.item.price}</p>
+        <button>Remove from cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
